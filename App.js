82defe3264caedef38de200f89c22e6315a9bef3
SimeonTu <45809363+SimeonTu@users.noreply.github.com>
2024-02-18 00:07:58 +0000

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Start from './components/Start';
+import Chat from './components/Chat';
+
+// Create the navigator
+const Stack = createNativeStackNavigator();
+
+const options = {
+  statusBarColor: '#521073',
+  headerStyle: {
+    backgroundColor: '#521073',
+  },
+  headerTintColor: '#fff',
+  headerTitleStyle: {
+    fontWeight: 'bold',
+  },
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Start"
+      >
+        <Stack.Screen
+          name="Start"
+          component={Start}
+          // use options prop to customize screen
+          options={{ ...options, headerShown: false }}
+        />
+        <Stack.Screen
+          name="Chat"
+          component={Chat}
+          options={options} // hide the top navigation bar
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
---
+++ components/Start.js
@@ -0,0 +1,191 @@
+import { useState, useEffect } from 'react';
+import { StyleSheet, View, Text, Button, TextInput, ImageBackground, Pressable, Dimensions, Appearance, KeyboardAvoidingView, Platform } from 'react-native';
+import BackgroundLight from '../assets/backgroundLight.svg'
+import BackgroundDark from '../assets/backgroundDark.svg'
+import FontAwesome6 from 'react-native-vector-icons/FontAwesome6';
+// import Background from './Background';
+
+const Start = ({ navigation }) => {
+    const [name, setName] = useState("");
+    const [mode, setMode] = useState(Appearance.getColorScheme())
+    const [bgColor, setBgColor] = useState("")
+    const [modeColor, setModeColor] = useState("")
+    const [screenHeight, setScreenHeight] = useState()
+
+    const toggleMode = () => {
+
+        if (mode === "light") {
+            setMode("dark")
+            setBgColor({ backgroundColor: "#521073" })
+            setModeColor({ backgroundColor: "white" })
+        } else {
+            setMode("light")
+            setBgColor({ backgroundColor: "white" })
+            setModeColor({ backgroundColor: "#521073" })
+        }
+    }
+
+    useEffect(() => {
+
+        setScreenHeight(Dimensions.get('window').height)
+
+        if (mode === "light") {
+            setBgColor({ backgroundColor: "white" })
+            setModeColor({ backgroundColor: "#521073" })
+        } else {
+            setBgColor({ backgroundColor: "#521073" })
+            setModeColor({ backgroundColor: "white" })
+        }
+
+        console.log(mode);
+        console.log(screenHeight);
+    }, [])
+
+
+    return (
+        <View style={[styles.container, bgColor]}>
+
+            {/* Light/Dark Mode Switch */}
+            <View style={[styles.modeButtonContainer, styles.shadow]}>
+                <Pressable
+                    style={[styles.modeButton, modeColor]}
+                    android_ripple={mode === "light" ? { color: 'white' } : { color: "#521073" }}
+                    onPress={toggleMode}
+                >
+                    <FontAwesome6 name="moon" size={24} color={mode === "light" ? "white" : "#521073"} solid />
+                </Pressable>
+            </View>
+
+            {/* Heading */}
+            <View
+                style={{...styles.headingContainer, ...modeColor, top: screenHeight * 0.2}}>
+
+                <Text style={mode === "light" ? { ...styles.heading, color: "white" } : { ...styles.heading, color: "#521073" }}>Welcome!</Text>
+            </View>
+
+
+            <View style={{ ...styles.background, height: screenHeight }}>
+                {mode === "light" ? (<BackgroundLight />) : (<BackgroundDark />)}
+            </View>
+
+            <View style={[styles.welcomeBox, styles.shadow, bgColor]}>
+
+                <View behavior="height" style={styles.textContainer}>
+                    <FontAwesome6 style={{ marginLeft: 2.5, marginRight: 10 }} name="user" size={20} color="grey" />
+                    <TextInput
+                        style={{ flex: 1 }}
+                        value={name}
+                        onChangeText={setName}
+                        placeholder='Your Name'
+                        placeholderTextColor={"rgba(0,0,0,0.4)"}
+                    />
+                </View>
+
+                <Pressable style={[styles.button, modeColor]}
+                    onPress={() => navigation.navigate('Chat', { name: name })}
+                >
+                    <Text style={mode === "light" ? { color: "white" } : { color: "#521073" }}>
+                        Start Chatting
+                    </Text>
+                </Pressable>
+            </View>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+    },
+    background: {
+        position: "absolute",
+        // minHeight: Dimensions.get('window').height,
+        height: "100%",
+        width: "100%",
+        // width/: "100%",
+        // height: "100%",
+        // alignSelf: "stretch",
+        // resizeMode: "contain",
+        top: 0,
+        right: 0,
+        bottom: 0,
+        left: 0,
+        zIndex: -1,
+    },
+    headingContainer: {
+        position: "absolute",
+        width: "80%",
+        padding: 10,
+        backgroundColor: "white",
+        // top: 200,
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        borderRadius: 7
+    },
+    heading: {
+        textAlign: "center",
+        fontSize: 40
+    },
+    textContainer: {
+        backgroundColor: "white",
+        flexDirection: 'row',
+        alignItems: "center",
+        width: "100%",
+        height: 60,
+        paddingLeft: 10,
+        paddingRight: 10,
+        paddingTop: 5,
+        paddingBottom: 5,
+        borderWidth: 1,
+        borderColor: "grey"
+    },
+    welcomeBox: {
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        width: "88%",
+        height: 175,
+        marginBottom: 40,
+        padding: 20,
+        backgroundColor: "white",
+    },
+    button: {
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100%",
+        height: 55,
+        backgroundColor: "#521073",
+        // borderRadius: 7
+    },
+    modeButtonContainer: {
+        position: "absolute",
+        top: 30,
+        right: 30,
+        overflow: "hidden",
+        borderRadius: 7
+    },
+    modeButton: {
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        width: 50,
+        height: 45,
+        elevation: 7
+    },
+    shadow: {
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 5,
+        elevation: 5,
+    }
+});
+
+export default Start;
